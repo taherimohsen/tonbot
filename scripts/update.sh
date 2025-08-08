ab821be4307e14afbcf78304c2a2282faf89e0a6
Mohsen Taheri <73648905+taherimohsen@users.noreply.github.com>
2025-08-08 04:51:50 +0330

    Update update.sh

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,5 +1,7 @@
-#!/bin/bash
+#!/usr/bin/env bash
+set -euo pipefail
 cd /root/tonbot
-git pull
+git pull --rebase
 npm install
 systemctl restart ton-webhook
+systemctl status ton-webhook --no-pager || true
